142eea0d3adcc8c87610c6f89199b0f004d82d38
harisumanth1637 <65695208+harisumanth1637@users.noreply.github.com>
2024-10-06 18:54:58 +0000

    Update TriadicClosure_Part3.scala

--- TriadicClosure_Part3.scala
+++ TriadicClosure_Part3.scala
@@ -9,8 +9,8 @@ object MutualFriendsFilterApp {
     val sc = new SparkContext(conf)
 
     // Hardcoded input and output paths
-    val inputHDFS = "hdfs://localhost:9000/user/hthtd/InputFolder/example.txt"
-    val outputHDFS = "hdfs://localhost:9000/user/hthtd/OutputFolder/Part3"
+    val inputHDFS = "hdfs://localhost:9000/user/hthtd/InputFolder/soc-LiveJournal1Adj.txt"
+    val outputHDFS = "hdfs://localhost:9000/user/hthtd/OutputFolder_Part3/"
 
     // Step 1: Read input data from HDFS
     val loadfile = sc.textFile(inputHDFS)
